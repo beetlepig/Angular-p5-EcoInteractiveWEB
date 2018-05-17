d1c76a86ebdfe5dc52a897e417f2cf51250824d2
sky_k <sky_karlos@hotmail.com>
2018-05-17 01:42:54 -0500

    prototipo component

--- src/app/prototipo/prototipo.component.ts
+++ src/app/prototipo/prototipo.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prototipo.component.css']
 })
 export class PrototipoComponent implements OnInit {
+  tituloSlide: string;
+  descripcionSlide: string;
+  subSlideNumber: number;
+  slideNumber: number;
 
-  constructor() { }
+  constructor() {
+    this.tituloSlide = 'EL SAPO';
+    this.descripcionSlide = 'Como prototipo se desarrollo una variante de un juego típico de colombia, el Sapo. Se adaptaron sensores, ' +
+      'un altavoz y una pantalla con el objetivo de brindar una interacción enriquecida al usuario, al tiempo que se le proporcionada ' +
+      'información sobre la labor medioambiental de Smurfit Kappa';
+    this.slideNumber = 0;
+    this.subSlideNumber = 0;
+  }
 
   ngOnInit() {
+    setInterval(() => {
+      if (this.slideNumber === 0) {
+        this.subSlideNumber++;
+        if (this.subSlideNumber > 1) {
+           this.subSlideNumber = 0;
+           this.slideNumber = 1;
+           this.tituloSlide = 'COMIENZO';
+           this.descripcionSlide = 'Cuando el usuario se ubique entre 1 m y 1.3 m enfrente del sapo, el juego iniciará mostrando una ' +
+             'introduccion de la labor ambiental de Smurfit Kappa, por medio del altavoz y la pantalla.';
+        }
+      }
+
+    }, 10000);
   }
 
 }
